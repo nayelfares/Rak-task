ba4018e6a1a0b479f3b79a3518292a6d6c732a4c
nayelalfares <nayel.fares@hotmail.com>
2022-04-24 22:06:55 +0400

    request model

--- app/features/onboarding/interfaces.ts
+++ app/features/onboarding/interfaces.ts
@@ -5,5 +5,17 @@ export interface ResponseGenerator {
   success?: boolean;
 }
 
+export interface LoginModel {
+  userId: string;
+  password: string;
+  os: string;
+  deviceName: string;
+  macAddress: string;
+  imei: string | null | undefined;
+  ipAddress: string;
+  longtude: string;
+  latitude: string;
+}
+
 export const successResponse = [200, 201];
 export const failedResponse = [400, 401];
--- app/features/onboarding/network/apis.ts
+++ app/features/onboarding/network/apis.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import {LoginModel} from '../interfaces';
 import {loginEndpoint} from './endpoints';
 
-export const loginApi = (payload: any) => {
+export const loginApi = (payload: LoginModel) => {
   return axios.post(loginEndpoint, payload);
 };
